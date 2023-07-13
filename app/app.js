3190fab7bf0ff05617bea944d6d7a6fc9c5ca03d
Joszuel <79731636+Joszuel@users.noreply.github.com>
2023-07-13 19:32:41 +0000

    Optimizar

--- app/app.js
+++ app/app.js
@@ -1,10 +1,11 @@
-const carta=document.querySelectorAll(".carta");
+const contenedor=document.querySelector(".contenedor");
 const img=document.querySelectorAll(".carta img");
 const info=document.querySelector(".info");
 const infoImg=document.querySelector(".info img");
 const botonCerrar=document.querySelector(".cerrar");
 const abrir = (i)=>{
-    infoImg.src=img[i].src;
+    console.log(i)
+    infoImg.src=i.src;
     info.style.opacity = "1";
     info.style.width = "95vw";
     info.style.height="95vh";
@@ -15,9 +16,11 @@ const cerrar = ()=>{
     info.style.height="0vh";
 }
 
-carta.forEach(function (carta,i){
-    carta.addEventListener('click',()=>abrir(i))
-})
+    contenedor.addEventListener('click',(a)=>{
+        if(a.target.localName==="img"){
+        abrir(a.target)
+        }
+    })
 
 botonCerrar.addEventListener("click",()=>
 cerrar()
